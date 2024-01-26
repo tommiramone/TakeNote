1051598c82aa45bcc3041e377c0b6842894b5b7e
tommiramone <88351561+tommiramone@users.noreply.github.com>
2024-01-26 16:05:08 -0300

    funcionalidad registro e inicio de sesion

--- app/src/main/java/com/curso/android/app/practica/takenote/Login.java
+++ app/src/main/java/com/curso/android/app/practica/takenote/Login.java
@@ -2,26 +2,63 @@ package com.curso.android.app.practica.takenote;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.curso.android.app.practica.takenote.database.DatabaseHelper;
+
 public class Login extends AppCompatActivity {
+    DatabaseHelper databaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        databaseHelper = new DatabaseHelper(this);
 
         Button buttonLogin = findViewById(R.id.buttonLogin);
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this, Home.class);
-                startActivity(intent);
+                EditText contraseniaEditText = findViewById(R.id.editTextPassword);
+                EditText emailEditText = findViewById(R.id.editTextUsername);
+                String contrasenia = contraseniaEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+
+                if (validarCredenciales(email, contrasenia)) {
+                    // Credenciales válidas, permitir acceso
+                    // Por ejemplo, iniciar una nueva actividad
+                    Intent intent = new Intent(Login.this, Home.class);
+                    startActivity(intent);
+                    finish(); // Finaliza la actividad de login para evitar volver atrás
+                } else {
+                    // Credenciales inválidas, mostrar mensaje de error
+                    Toast.makeText(Login.this, "Credenciales inválidas", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
     }
+
+    public boolean validarCredenciales(String email, String contrasenia){
+        boolean credencialesValidas = databaseHelper.login(email, contrasenia);
+
+        if (credencialesValidas) {
+            Log.d("LoginActivity", "Inicio de sesión exitoso para el usuario: " + email);
+        } else {
+            Log.d("LoginActivity", "Inicio de sesión fallido para el usuario: " + email);
+        }
+
+        return credencialesValidas;
+    }
+
+
 }
+
+
--- app/src/main/java/com/curso/android/app/practica/takenote/database/DatabaseHelper.java
+++ app/src/main/java/com/curso/android/app/practica/takenote/database/DatabaseHelper.java
@@ -34,7 +34,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         String createTableQuery = "CREATE TABLE notas (_id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, cuerpo TEXT)";
         db.execSQL(createTableQuery);
 
-        String createUsuariosTableQuery = "CREATE TABLE usuarios (_id INTEGER PRIMARY KEY, nombre TEXT, email TEXT, contraseña TEXT)";
+        String createUsuariosTableQuery = "CREATE TABLE usuarios (_id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, email TEXT, contraseña TEXT)";
         db.execSQL(createUsuariosTableQuery);
 
         String createPapeleraTableQuery = "CREATE TABLE papelera (_id INTEGER PRIMARY KEY AUTOINCREMENT, idNota INTEGER, titulo TEXT, cuerpo TEXT)";
@@ -45,20 +45,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     }
 
-    public void reiniciarPapelera() {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        try {
-            db.delete("papelera", null, null);
-            Log.d("DatabaseHelper", "Papelera reiniciada exitosamente");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (db.isOpen()) {
-                db.close();
-            }
-        }
-    }
+//    public void reiniciarPapelera() {
+//        SQLiteDatabase db = this.getWritableDatabase();
+//
+//        try {
+//            db.delete("papelera", null, null);
+//            Log.d("DatabaseHelper", "Papelera reiniciada exitosamente");
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (db.isOpen()) {
+//                db.close();
+//            }
+//        }
+//    }
 
     public void insertarNota(String titulo, String cuerpo) {
         SQLiteDatabase db = this.getWritableDatabase();
@@ -109,6 +109,45 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         public String getCuerpo() {
             return cuerpo;
         }
+    }
+
+    public class Usuario {
+        private int id;
+        private String nombre;
+        private String email;
+        private String contrasenia;
+
+        public Usuario(String nombre, String contrasenia, String email) {
+            this.nombre = nombre;
+            this.email = email;
+            this.contrasenia = contrasenia;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public String getEmail(){
+            return email;
+        }
+
+        public String getContrasenia(){
+            return contrasenia;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public void setContrasenia(String contrasenia) {
+            this.contrasenia = contrasenia;
+        }
+
+
     }
 
     public List<Nota> obtenerNotas(String tipo) {
@@ -264,6 +303,46 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
+    public void registrarUsuario(String nombre, String email, String contrasenia){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("nombre", nombre);
+        values.put("email", email);
+        values.put("contraseña", contrasenia);
+
+        long resultado = db.insert("usuarios", null, values);
+
+        if (resultado != -1) {
+            Log.d("DatabaseHelper", "Usuario registrado correctamente: " + nombre);
+        } else {
+            Log.e("DatabaseHelper", "Error al registrar el usuario: " + nombre);
+        }
+
+        db.close();
+    }
+
+    public boolean login(String email, String contrasenia) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = { "email" };
+        String selection = "email=? and contraseña=?";
+        String[] selectionArgs = { email, contrasenia };
+        Cursor cursor = null;
+        try {
+            cursor = db.query("usuarios", columns, selection, selectionArgs, null, null, null);
+            int count = cursor.getCount();
+            return count > 0;
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            db.close();
+        }
+    }
+
 
 
 }
+
+
+
